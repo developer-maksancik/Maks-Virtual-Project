e9bfc4e512e6bb6e4a0a7a2676e8ddd667d35ad4
maks-1212 <dekabri2316@gmail.com>
2026-01-07 21:49:56 +0300

    пофикшен баг в выборе языка в регистрации

--- virtyalProjekt.py
+++ virtyalProjekt.py
@@ -62,16 +62,15 @@ print("2.English")
 U = input("> ")
 if U == "1":
     print("успешно!")
-if U == "2":
+elif U == "2":
     print("success!")
     lang = True
 else:
-    print("error")
     print("выберете язык/select language")
     print("1.руский")
     print("2.English")
     U = input("> ")
-
+    
 print("здраствуйте")
 print("чтобы пользватся виртуальным сервером необходимо зарегестрироватся")
 Q = input("ваше имя: ")
